7a0f9f45452b3a9e9312fb3c3ba2fbe7cc85c547
Lazar Jankovic <jlazar997@gmail.com>
2022-04-08 15:57:57 +0200

    Updates

--- src/middleware/require-auth.ts
+++ src/middleware/require-auth.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import { NotAuthorizerError } from '../errors/not-authorized-error';
 
 export const requireAuth = (
